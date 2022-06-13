494437c1eda66a060f7dc7e06b01692e47c584a5
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-13 20:26:12 +0600

    add some function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// // welcome website
+// alert('Welcome Mr/Mis')
+
 function MyCustomFunction() {
     alert('Welcome to javascript!')
 }
@@ -76,5 +79,44 @@ student.address = 'Rupatali,Battala'
 console.log(student);
 
 // can add new element
-student.phone = ('01741445482')
+student.phone = (typeof '01741445482')
 console.log(student);
+
+// Function
+
+// Convert Fahrenheit to Celsius:
+function toCelsius(fahrenheit) {
+    return (5 / 9) * (fahrenheit - 32);
+}
+document.getElementById("result").innerHTML = toCelsius(80);
+
+// create today date
+function timeIs() {
+    document.getElementById('result2').innerHTML = new Date();
+}
+
+// Replacing String Content
+function changeText() {
+    let text = document.getElementById('result3').innerHTML;
+    document.getElementById('result3').innerHTML = text.replace("Microsoft", "W3Schools");
+}
+
+// // string convert to upper case
+// function changeUpperCase() {
+//     let text2 = document.getElementById('result4').innerHTML;
+//     document.getElementById('result4').text2.toUpperCase();
+// }
+
+// String split
+let string = "Mehedi,Hasan,Sobuz";
+const myArray = string.split(",");
+document.getElementById('result4').innerHTML = myArray[0]
+
+// indexOf
+let text2 = "Please locate where 'locate' occurs"
+document.getElementById('result5').innerHTML = text2.indexOf("where")
+document.getElementById('result6').innerHTML = text2.lastIndexOf("where")
+document.getElementById('result7').innerHTML = text2.startsWith("locate", 7)
+document.getElementById('result8').innerHTML = text2.endsWith("occurs")
+document.getElementById('result9').innerHTML = text2.search("locate")
+document.getElementById('result10').innerHTML = text2.match("ate")
